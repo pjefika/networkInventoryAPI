de5852cbc9d8c2f0cd7c799e190870890b3f2fc5
Henrique Merlin <henmerlin@gmail.com>
2018-03-08 17:35:47 -0300

    + entity SigresMetalico + DAO

---
+++ networkInventoryAPI/src/main/java/br/net/gvt/efika/networkInventoryAPI/dao/NetworkInventorySigresMetalicoDAO.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.net.gvt.efika.networkInventoryAPI.dao;
+
+import javax.persistence.Query;
+import br.net.gvt.efika.networkInventoryAPI.model.entity.NetworkInventorySigresMetalico;
+
+/**
+ *
+ * @author G0042204
+ */
+public class NetworkInventorySigresMetalicoDAO extends AbstractHibernateDAO {
+
+    public NetworkInventorySigresMetalicoDAO() {
+    }
+
+    public NetworkInventorySigresMetalico consultar(String param1) throws Exception {
+        try {
+            Query query = entity().createQuery("FROM NetworkInventorySigresMetalico i "
+                    + "WHERE "
+                    + "("
+                    + "i.nrc =:param1 "
+                    + "OR i.terminal =:param1 "
+                    + ")");
+            query.setParameter("param1", param1);
+            return (NetworkInventorySigresMetalico) query.getSingleResult();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.close();
+        }
+    }
+
+    public NetworkInventorySigresMetalico consultarDslam(String nomeDslam) throws Exception {
+        try {
+            Query query = entity().createQuery("FROM NetworkInventorySigresMetalico i WHERE i.nomeDslam =:param1");
+            query.setParameter("param1", nomeDslam);
+            query.setMaxResults(1);
+            return (NetworkInventorySigresMetalico) query.getSingleResult();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.close();
+        }
+    }
+
+}
--- networkInventoryAPI/src/main/java/br/net/gvt/efika/networkInventoryAPI/model/entity/NetworkInventorySigresFibra.java
+++ networkInventoryAPI/src/main/java/br/net/gvt/efika/networkInventoryAPI/model/entity/NetworkInventorySigresFibra.java
@@ -11,7 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
